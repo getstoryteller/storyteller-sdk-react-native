a6d3d4a9c55e6e70fe93b8947c068dc24e4c58c8
jxie93 <zhihao420@hotmail.com>
2021-10-05 12:52:27 +0100

    Add props comments docs

--- App.tsx
+++ App.tsx
@@ -45,6 +45,53 @@
                style={styles.storyContainer}
                apiKey={"test-key"}
                externalUserId={"test-user"}
+
+              // learn more - https://docs.getstoryteller.com/documents/react-native-sdk/StorytellerComponent
+
+              // Style of the cell in the row - can be 'round' or 'square'
+              // cellType={} 
+
+              // Height scale of the cell in the row - default is 1
+              //  cellScale={} 
+
+              // Horizontal spacing of the cells in the row - default is 0
+              // cellSpacing={} 
+
+              // Left inset of cells in the row - default is 0
+              // leftInset={} 
+
+              // Right inset of cells in the row - default is 0
+              // rightInset={} 
+
+              // SDK specific appearance customization
+              // theme={} 
+
+              // Callback for initialization errors
+              // onInitializationFailure={} 
+
+              // Callback for when the SDK starts loading story data
+              // onDataLoadStarted={} 
+
+              // Callback for when the SDK finishes loading story data
+              // onDataLoadCompleted={} 
+
+              // Callback for when the user exits the story player view
+              // onStoryDismiss={} 
+
+              // Callback for when the user interacts with the SDK
+              // onUserActivityOccurred={} 
+
+              // Callback for when the user swipes up on a page configured as inApp link
+              // onUserSwipedUpToApp={} 
+
+              // Callback for when a tile in the row becomes visible
+              // onTileBecameVisible={} 
+
+              // Callback for when the SDK needs ads to render, if your integration uses ads
+              // onGetAdsForRow={} 
+
+              // Callback for when the SDK opens a deep link and reports success
+              // onOpenDeeplinkResult={} 
              />
          </View>
        </ScrollView>
