61bfadc890a7eed01e6326c8322cdbed1e6f6482
Bartosz Stelmaszuk <stelmaszuk.b@gmail.com>
2022-02-03 11:52:35 +0100

    update SDK to 6.0.2 version

--- App.tsx
+++ App.tsx
@@ -10,28 +10,79 @@
 
  import React from 'react';
  import {
+   Appearance,
    SafeAreaView,
    ScrollView,
    StatusBar,
    StyleSheet,
-   useColorScheme,
    View,
+   NativeEventEmitter,
  } from 'react-native';
  
- import Storyteller from '@getstoryteller/react-native-storyteller-sdk';
- 
+ import Storyteller, { StorytellerRowView, StorytellerGridView } from '@getstoryteller/react-native-storyteller-sdk';
+ const { ON_USER_ACTIVITY_OCCURED, USER_SWIPED_UP_TO_APP, GET_ADS_FOR_LIST } = Storyteller.getConstants();
+ import type {
+  EventType,
+  UserActivityData,
+  ClientStory,
+  ClientAd,
+} from '@getstoryteller/react-native-storyteller-sdk';
+
  import {
    Colors,
  } from 'react-native/Libraries/NewAppScreen';
  
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+class App extends React.Component<any, any> {
+
+   private rowRef?: StorytellerRowView;
+   private gridRef?: StorytellerGridView;
+
+   constructor(props: any) {
+    super(props);
+    const storytellerEvt = new NativeEventEmitter(Storyteller);
+    storytellerEvt.addListener(ON_USER_ACTIVITY_OCCURED, this._onUserActivityOccurred); // Callback for when the user interacts with the SDK
+    storytellerEvt.addListener(USER_SWIPED_UP_TO_APP, this._onUserSwipedUpToApp); // Callback for when the user swipes up on a page configured as inApp link
+    storytellerEvt.addListener(GET_ADS_FOR_LIST, this._onGetAdsForList); // Callback for when the SDK needs ads to render, if your integration uses ads
+    this._initializeStoryteller()
+  }
+
+  _onUserActivityOccurred = (body: {
+    type: EventType;
+    data: UserActivityData;
+  }) => {
+    console.log(
+      `ActivityOccurred\n` +
+        `type: ${body.type}, data: ${JSON.stringify(body.data)}`
+    );
+  };
+
+  _onUserSwipedUpToApp = (body: { swipeUpUrl: string }) => {
+    console.log(`UserSwipedUpToApp\n` + `swipeUpUrl: ${body.swipeUpUrl}`);
+  };
+
+  _onGetAdsForList = (body: { stories: [ClientStory] }) => {
+    console.log(`GetAdsForList\n` + `stories: ${JSON.stringify(body.stories)}`);
+  };
+
+  _initializeStoryteller = () => {
+    Storyteller.initialize(
+      "test-key",
+      "test-user",
+      '', // URL of custom instance to run API
+      [], // Categories for which stories will be preloaded
+      (callback: { result: Boolean, message: string }) => {
+        console.log(`result: ${callback.result} message: ${callback.message}`);
+        this._reloadDataIfNeeded();
+      }
+    );
+  };
+
+  _reloadDataIfNeeded = () => {
+    if (this.rowRef) this.rowRef.reloadData();
+    if (this.gridRef) this.gridRef.reloadData();
+  };
  
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-     flex: 1
-   };
- 
+  render() {
    return (
      <SafeAreaView style={backgroundStyle}>
        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -43,21 +94,16 @@
              flex: 1,
              backgroundColor: isDarkMode ? Colors.black : Colors.white,
            }}>
-             <Storyteller
+             <StorytellerRowView
+               ref={(ref: any) => {
+                  if (ref) this.rowRef = ref;
+               }}
                style={styles.storyContainer}
-               apiKey={"test-key"}
-               externalUserId={"test-user"}
 
               // learn more - https://docs.getstoryteller.com/documents/react-native-sdk/StorytellerComponent
-
-              // Categories for which stories will be preloaded
-              // categoriesToPreload={}
               
               // Categories for which row will display storie
               // categories={} 
-              
-              // URL of custom instance to run API
-              // customInstanceHost={}
 
               // Style of the cell in the row - can be 'round' or 'square'
               // cellType={} 
@@ -65,17 +111,14 @@
               // Horizontal spacing of the cells in the row - default is 0
               // cellSpacing={} 
 
-              // Left inset of cells in the row - default is 0
-              // leftInset={} 
+              // Start inset of cells in the row - default is 0
+              // startInset={} 
 
-              // Right inset of cells in the row - default is 0
-              // rightInset={} 
+              // End inset of cells in the row - default is 0
+              // endInset={} 
 
               // SDK specific appearance customization
               // theme={} 
-
-              // Callback for initialization errors
-              // onInitializationFailure={} 
 
               // Callback for when the SDK starts loading story data
               // onDataLoadStarted={} 
@@ -86,17 +129,8 @@
               // Callback for when the user exits the story player view
               // onStoryDismiss={} 
 
-              // Callback for when the user interacts with the SDK
-              // onUserActivityOccurred={} 
-
-              // Callback for when the user swipes up on a page configured as inApp link
-              // onUserSwipedUpToApp={} 
-
               // Callback for when a tile in the row becomes visible
               // onTileBecameVisible={} 
-
-              // Callback for when the SDK needs ads to render, if your integration uses ads
-              // onGetAdsForRow={} 
 
               // Callback for when the SDK opens a deep link and reports success
               // onOpenDeeplinkResult={} 
@@ -105,7 +139,16 @@
        </ScrollView>
      </SafeAreaView>
    );
- };
+  }
+
+};
+
+const isDarkMode = Appearance.getColorScheme() === 'dark';
+
+const backgroundStyle = {
+  backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  flex: 1
+};
  
  const styles = StyleSheet.create({
    storyContainer: {
